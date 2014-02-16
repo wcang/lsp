eb9a1a43ecb2c51374974200f97fa487c88f3cc6
Ang Way Chuang <wcang79@gmail.com>
2014-02-16 14:40:45 +0800

    Add an example of forking child processes. Zombies await

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: error copy open
+all: error copy open fork
 
 error: error.c
 	gcc -g -O0 -Wall -o error error.c
@@ -9,5 +9,8 @@ copy: copy.c
 open: open.c
 	gcc -g -O0 -Wall -o open open.c
 
+fork: fork.c
+	gcc -g -O0 -Wall -o fork fork.c
+
 clean:
-	rm -rf error copy
+	rm -rf error copy open fork
---
+++ fork.c
@@ -0,0 +1,57 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+static void child_process(int fd)
+{
+	int i;
+	char buffer[100];
+
+	printf("PID: %u\n", getpid());
+
+	for (i = 0; i < 10; ++i) {
+		snprintf(buffer, sizeof(buffer), "%u: %d\n", getpid(), i);
+		write(fd, buffer, strlen(buffer));
+	}
+
+	close(fd);
+	exit(0);
+}
+
+
+int main(int argc, char * argv[])
+{
+	int i;
+	int fd;
+	int child_pid[10];
+
+	fd = open("child.log", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+
+	if (fd == -1) {
+		fprintf(stderr, "Hi there... something wrong with opening a log file for your offspring: %s\n", strerror(errno));
+		exit(1);
+	}
+
+	for (i = 0; i < 10; ++i) {
+		child_pid[i] = fork();
+
+		if (child_pid[i] == 0) { /* I'm the child, let do something else */
+			child_process(fd);
+		}
+		else if (child_pid[i] == -1) {
+			fprintf(stderr, "Oops... Labour pain. Cannot give birth to child: %s\n", strerror(errno));
+			exit(1);
+		}
+		/* parent proceeds with the loop */	
+	}
+
+	close(fd);
+	pause();
+	return 0;
+}
+
